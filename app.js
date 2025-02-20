dc4efe32e9a9e0aee639ef729617d031eb648320
AtsuID <atsutrial1@gmail.com>
2025-02-20 09:50:53 +0800

    renev

--- app.js
+++ app.js
@@ -20,32 +20,3 @@ var swiper = new Swiper(".blogSwiper", {
     },
   },
 });
-
-// Scroll Reveal
-ScrollReveal().reveal('.hero__image', { duration: 750, delay: 100, origin: "bottom", distance: "25px" });
-
-ScrollReveal().reveal('#value_1', { duration: 750, delay: 100, origin: "bottom", distance: "25px" });
-
-ScrollReveal().reveal('#value_2', { duration: 750, delay: 300, origin: "bottom", distance: "25px" });
-
-ScrollReveal().reveal('#value_3', { duration: 750, delay: 300, origin: "bottom", distance: "25px" });
-
-ScrollReveal().reveal('#value_4', { duration: 750, delay: 200, origin: "bottom", distance: "25px" });
-  
-ScrollReveal().reveal('#provide_1', { duration: 750, delay: 100, origin: "bottom", distance: "25px" });
-
-ScrollReveal().reveal('#provide_2', { duration: 750, delay: 200, origin: "bottom", distance: "25px" });
-
-ScrollReveal().reveal('#provide_3', { duration: 750, delay: 300, origin: "bottom", distance: "25px" });
-
-ScrollReveal().reveal('.blog_1', { duration: 750, delay: 100, origin: "bottom", distance: "25px" });
-
-ScrollReveal().reveal('.blog_2', { duration: 750, delay: 200, origin: "bottom", distance: "25px" });
-
-ScrollReveal().reveal('.blog_3', { duration: 750, delay: 300, origin: "bottom", distance: "25px" });
-
-ScrollReveal().reveal('.story', { duration: 750, delay: 100, origin: "bottom", distance: "25px" });
-
-ScrollReveal().reveal('.reminder__text', { duration: 750, delay: 100, origin: "bottom", distance: "25px" });
-
-ScrollReveal().reveal('.default__button', { duration: 750, delay: 200, origin: "bottom", distance: "25px" });
